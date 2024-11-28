28c20f454acd21a06b86fd00ce2f164df0271f1e
Omid Reza Heidari <omid.orh@gmail.com>
2024-11-28 12:57:04 -0500

    Add files via upload

---
+++ train/butterfly.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -J OmnButter
+#SBATCH --mem=200GB
+#SBATCH --gpus=1
+#SBATCH --mail-type=NONE
+#SBATCH --mail-user=omid.orh@gmail.com
+#SBATCH --partition=all
+#SBATCH --chdir=../../
+#SBATCH -o ./jobs/output/Omnimotion-Butterfly-%j.out
+
+module load anaconda
+eval "$(conda shell.bash hook)"
+conda activate /home/o_heidar/omnimotionenv
+python train.py --config configs/default.txt --data_dir data/data/butterfly
